a3c8555acbd7995821109f7d2bdf810eb84dbb42
1havran <havran.init0@gmail.com>
2018-01-13 18:02:23 +0000

    metron

--- recipes/metron-master.sh
+++ recipes/metron-master.sh
@@ -2,6 +2,7 @@
 cd
 git clone https://github.com/apache/metron.git
 cd metron
+git checkout 2dd01b17782af7756776b8e07afe3661ac19bb0a
 mvn clean package -DskipTests -Pbuild-rpms
 cd metron-deployment/packaging/docker/rpm-docker/RPMS/noarch/
 rpm -Uvh metron*rpm
@@ -9,6 +10,7 @@ rpm -Uvh metron*rpm
 echo '127.0.0.1 node1' >> /etc/hosts
 echo 'listeners=PLAINTEXT://:6667' >> /home/kafka/kafka/config/server.properties
 sed -i "s/topology.auto-credentials=['']/topology.auto-credentials=/g" /usr/metron/*/config/elasticsearch.properties
+echo 'cluster.name: metron' >> /home/elasticsearch/elasticsearch/config/elasticsearch.yml
 
 ln -s /home/storm/storm/bin/storm /usr/bin/storm
 ln -s /home/zookeeper/zookeeper/bin/zkCli.sh /usr/bin/zookeeper-client
